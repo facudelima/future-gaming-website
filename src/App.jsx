c640b7591f27aeb5940bb3f93adb04f7447c5a98
facudelima <78700250+facudelima@users.noreply.github.com>
2026-02-20 19:07:38 -0300

    Fix formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+ 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
